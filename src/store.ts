c10daa1b4807e2a7ac0736e52c0926be1f5d0fcc
Chris Hopkins <chris@liquid-state.com>
2018-10-18 10:07:52 +1000

    Make Identity Store generic over the type of data it stores so that credential providers with more nuanced requirements for stored data than ISerialisedIdentity can use it easily

--- src/store.ts
+++ src/store.ts
@@ -1,37 +1,39 @@
 import { KeyValueStore, Key } from '@liquid-state/iwa-keyvalue';
+import { Value } from '@liquid-state/iwa-keyvalue/dist/key';
 
 export interface ISerialisableIdentity {
   identity: string | null;
   credentials: object | null;
 }
 
-export interface IIdentityStore {
-  store(service: string, identity: ISerialisableIdentity): void;
-  fetch(service: string): Promise<ISerialisableIdentity>;
+export interface IIdentityStore<T extends Value = ISerialisableIdentity> {
+  store(service: string, identity: T): void;
+  fetch(service: string): Promise<T>;
 }
 
 export interface IIdentityStoreDelegate {
   setPermissionsForKey(key: Key): void;
 }
 
-export default class IdentityStore implements IIdentityStore {
+export default class IdentityStore<T extends Value = ISerialisableIdentity>
+  implements IIdentityStore<T> {
   private readonly BASE_KEY = 'identity';
 
   constructor(private keyValueStore: KeyValueStore, private delegate: IIdentityStoreDelegate) {}
 
-  store(service: string, identity: ISerialisableIdentity): void {
+  store(service: string, identity: T): void {
     let keyName = `${this.BASE_KEY}.${service}`;
     let key = new Key(keyName, identity);
     this.delegate.setPermissionsForKey(key);
     this.keyValueStore.set(key);
   }
 
-  async fetch(service: string): Promise<ISerialisableIdentity> {
+  async fetch(service: string): Promise<T> {
     let keyName = `${this.BASE_KEY}.${service}`;
     let key = await this.keyValueStore.get(keyName);
     if (!key.value) {
-      return { identity: null, credentials: null };
+      throw '';
     }
-    return key.decodeValue() as ISerialisableIdentity;
+    return key.decodeValue() as T;
   }
 }
